976b5fc57f6c09d588f4bc23899b38ead51d9ac7
Rona Bompa <rona.bompa@endava.com>
2022-03-07 22:47:08 +0200

    initial commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Rona Bompa on 07.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
