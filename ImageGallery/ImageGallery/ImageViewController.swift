316c7d75bb001764c3968f3308d0b4a676d8ed94
Rona Bompa <rona.bompa@endava.com>
2022-03-10 21:47:36 +0200

    scrollView

--- ImageGallery/ImageGallery/ImageViewController.swift
+++ ImageGallery/ImageGallery/ImageViewController.swift
@@ -9,29 +9,83 @@ import UIKit
 
 class ImageViewController: UIViewController {
 
-    var galleryImage: ImageGallery.Image? {
+    @IBOutlet weak var scrollView: UIScrollView! {
         didSet {
-//            if galleryItem != nil {
-//                fetchImagE(galleryImage!.imageURL)
-//            }
+            scrollView.delegate = self
         }
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    /// the image to display
+    var galleryImage: ImageGallery.Image? {
+        didSet {
+            if galleryImage != nil {
+                fetchImage(galleryImage!.url)
+            }
+        }
     }
-    
 
-    /*
-    // MARK: - Navigation
+    /// fetch image
+    private func fetchImage(_ url: URL) {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        DispatchQueue.global(qos: .userInitiated).async {
+            if let data = try? Data(contentsOf: url.imageURL) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self.imageView = UIImageView(image: image)
+                    }
+                }
+            }
+        }
     }
-    */
+
+    /// the imageView to display
+    private var imageView: UIImageView? {
+        didSet {
+            if imageView != nil {
+                // adapt view to fit image
+                imageView!.sizeToFit()
+
+                // add imageView to scrollView
+                scrollView.addSubview(imageView!)
+
+                // update scrollView accordingly
+                updateScrollView()
+            } else {
+                scrollView.subviews.forEach { $0.removeFromSuperview() }
+            }
+        }
+    }
+
+    private func updateScrollView() {
+        if imageView != nil {
+            let imageSize = imageView?.image?.size ?? CGSize.zero
+            let displaySize = scrollView.bounds.size
+            // scrollview content-size must fit the image
+            scrollView.contentSize = imageSize
+
+            // a scale that will fit the image on the screen
+            let zoommScaleToFitsWholeImage = min(displaySize.width/imageSize.width,displaySize.height/imageSize.height)
+
+            scrollView.minimumZoomScale = zoommScaleToFitsWholeImage
+            scrollView.maximumZoomScale = zoommScaleToFitsWholeImage
+            scrollView.maximumZoomScale = 3.0
+        }
+    }
+
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        updateScrollView()
+    }
+
+}
+
+
+extension ImageViewController: UICollectionViewDelegate {
+
+    /// the view for zooming in the scrollView
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return scrollView.subviews.first as? UIImageView
+    }
 
 }
